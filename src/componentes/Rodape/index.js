402f86672dad5f03c362decdf48ebb21adeb584a
Ítalo Souza <95109439+RagnaReal@users.noreply.github.com>
2025-03-11 19:19:20 -0300

    Add files via upload
    Atualização do código adicionando novas funções:
    - Deletar e criar grupo
    - Alterar cores dos cards de grupo e colaborador

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -1,34 +1,35 @@
-import './Rodape.css'
+
+import './rodape.css'
 
 const Rodape = () => {
     return (<footer className="footer">
-        <section>
-            <ul>
-                <li>
-                    <a href="https://www.facebook.com/" rel="noreferrer" target="_blank">
-                        <img src="/imagens/facebook.png" alt="" />
-                    </a>
-                </li>
-                <li>
-                    <a href="https://x.com/home?lang=pt-br" rel="noreferrer" target="_blank">
-                        <img src="/imagens/x.png" alt="" />
-                    </a>
-                </li>
-                <li>
-                    <a href="https://www.instagram.com/" rel="noreferrer" target="_blank">
-                        <img src="/imagens/instagram.png" alt="" />
-                    </a>
-                </li>
-            </ul>
-        </section>
-        <section>
-            <img src="/imagens/logo.png" alt="" />
-        </section>
-        <section>
-            <p>
-                Desenvolvido por Alura.
-            </p>
-        </section>
+       <section>
+       <ul>
+            <li>
+                <a href="facebook.com" target="_blank">
+                    <img src="/imagens/facebook.png" alt="" />
+                </a>
+            </li>
+            <li>
+                <a href="twitter.com" target="_blank">
+                    <img src="/imagens/twitter.png" alt="" />
+                </a>
+            </li>
+            <li>
+                <a href="instagram.com" target="_blank">
+                    <img src="/imagens/instagram.png" alt="" />
+                </a>
+            </li>
+        </ul>
+       </section>
+       <section>
+       <img src="/imagens/logo.png" alt="" />
+       </section>
+       <section>
+        <p>
+            Desenvolvido por Alura.
+        </p>
+       </section>
     </footer>)
 }
 
